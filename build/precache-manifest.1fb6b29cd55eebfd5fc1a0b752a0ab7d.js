19923fff95cd212b976a5d803e230d67535ae069
Eray Kaya <eraykaya121@gmail.com>
2019-08-26 21:31:55 +0300

    build

---
+++ build/precache-manifest.1fb6b29cd55eebfd5fc1a0b752a0ab7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c419591c2feab04eca58347641e2e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca8b388361b9df46076c",
+    "url": "/static/css/main.393ddd0c.chunk.css"
+  },
+  {
+    "revision": "d1daab78f0e0bed7d8e8",
+    "url": "/static/js/2.a7e4b9c0.chunk.js"
+  },
+  {
+    "revision": "ca8b388361b9df46076c",
+    "url": "/static/js/main.08606ed8.chunk.js"
+  },
+  {
+    "revision": "c8a88381e707d931241e",
+    "url": "/static/js/runtime~main.597d169e.js"
+  }
+]);
